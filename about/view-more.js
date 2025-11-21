ed2339648e937186a4d7c146e83d2130d022029b
fukushi-mako <fukushi@nextreservation.jp>
2025-11-21 18:23:34 +0900

    しいのみ会について追加（途中）

---
+++ about/view-more.js
@@ -0,0 +1,8 @@
+const view_btn = document.querySelector("#view-more");
+
+view_btn.addEventListener("click", (e) => {
+    const dl = document.querySelector("#ayumi-list");
+    dl.classList.toggle("open");
+    
+    view_btn.textContent = dl.classList.contains("open") ? "閉じる" : "もっと見る";
+});
